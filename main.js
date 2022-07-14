a756e95d2d4b381094e5f28e445a65d557e9830a
pjha9500 <pjha9500@gmail.com>
2022-07-14 14:40:15 +0530

    pagenotfoundcommit

--- main.js
+++ main.js
@@ -11,5 +11,7 @@ app.use(bodyparser.urlencoded({extended:false}));
 
 app.use(adminRoute);
 app.use(shopRoute);
-
+app.use((req,res,next)=>{
+    res.send('<h1>Page not found</h1>');
+})
 app.listen(3000);
